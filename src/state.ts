3ae69184e96d2d8dd1523946a90e3749df2ae41e
thlamz <thlamenza@hotmail.com>
2023-10-31 00:06:45 -0300

    Direção e velocidade separadamente

--- src/state.ts
+++ src/state.ts
@@ -7,13 +7,14 @@ import Wind from "./wind";
  */
 export type StateArray = number[]
 
-export const STATE_SIZE = 6
+export const STATE_SIZE = 7
 
 export function collectState(drone: DroneEntity, _wind: Wind): StateArray {
     const state: StateArray = []
     drone.physics.transformNode.absolutePosition.toArray(state)
     // drone.physics.transformNode.rotation.toArray(state, state.length)
-    drone.physics.body.getLinearVelocity().toArray(state, state.length)
+    drone.physics.body.getLinearVelocity().normalizeToNew().toArray(state, state.length)
+    state.push(drone.physics.body.getLinearVelocity().length())
     // drone.physics.body.getAngularVelocity().toArray(state, state.length)
     // state.push(wind.windDirection)
 
